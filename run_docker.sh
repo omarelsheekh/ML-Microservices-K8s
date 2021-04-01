c03dd3832868c1660bd6eecee6e796f251b16ad8
root <root@dev.2ah4zd22pbbeza4fsxe30fd5vd.zx.internal.cloudapp.net>
2021-04-01 17:45:03 +0000

    docker

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build -t omarelsheekh/mldevops .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run -dp 8000:80 omarelsheekh/mldevops
