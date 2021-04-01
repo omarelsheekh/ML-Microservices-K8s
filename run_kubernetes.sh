f60565225f9cf0ef4a89b2c1cd140121f9c31b2f
Ubuntu <omar@dev.2ah4zd22pbbeza4fsxe30fd5vd.zx.internal.cloudapp.net>
2021-04-01 19:31:54 +0000

    k8s

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=omarelsheekh/mldevops
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run model --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pod
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl expose pod model --port=80 --target-port=80
